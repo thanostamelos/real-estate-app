0fe32ed041981b097cabe357b41d5cca6b8b2093
ThanasisStamelos <TStamelos@visionit.gr>
2026-04-24 19:41:16 +0300

    Logo and image fixes

--- src/layout/Header/components/LogoSection.js
+++ src/layout/Header/components/LogoSection.js
@@ -10,14 +10,14 @@ const LogoSection = () => {
     return (
         <Box sx={{display: 'flex', alignItems: 'center', gap: 1.5, ml: -1, pr: 2, cursor: 'pointer'}} onClick={goHome}>
             <Avatar
-                src="/cinema_app_logo_transparent.png"
+                src="/real-estate.png"
                 alt="Cinema App"
                 variant="rounded"
                 sx={{width: 40, height: 40, backgroundColor: 'transparent', zIndex: 1000}}
             />
             <Box>
                 <Typography variant="h6" sx={{fontWeight: 800, lineHeight: 1.1}}>
-                    Cinema Booking
+                    Real Estate
                 </Typography>
             </Box>
         </Box>
